695abbc5b756207a69e46348bc146ad2965cf73f
Hasnaa El masry <hasnaa.elmasry94@gmail.com>
2023-04-29 11:03:29 +0300

    adjust layout and other components

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar'
 const Layout = ({children}) => {
     return (
         <>
-            {/* <Navbar /> */}
+            <Navbar />
             {children}
             <Footer />
         </>
